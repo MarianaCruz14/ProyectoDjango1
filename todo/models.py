4bc6d4a3199a558033559ab64945e232402f945b
MarianaCruz14 <mrncruzgomez@gmail.com>
2025-04-08 23:07:31 -0500

    OK

--- todo/models.py
+++ todo/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 class Tarea(models.Model):
     tarea=models.CharField(max_length=100)
+    descripcion=models.TextField(blank=True)
     def __str__(self):
         return self.tarea
-    
+
+
